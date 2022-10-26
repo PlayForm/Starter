8c2a7198b52fce8d695f5455c6b3c06905a077ab
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-27 01:17:53 +0300

    squash!

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	content: [
+		"./public/**/*.html",
+		"./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}",
+	],
+	darkMode: "media",
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						a: {
+							"font-weight": "400",
+						},
+					},
+				},
+			},
+		},
+	},
+	variants: {},
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/line-clamp"),
+		require("@tailwindcss/aspect-ratio"),
+	],
+};
