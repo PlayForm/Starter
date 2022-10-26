5674a713b4283f19c91298aa639cd8e214130cb0
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-27 01:18:01 +0300

    squash!

---
+++ src/scripts/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "",
+	appId: "",
+	authDomain: "",
+	databaseURL: "",
+	measurementId: "",
+	messagingSenderId: "",
+	projectId: "",
+	storageBucket: "",
+};
+
+initializeApp(firebaseConfig);
