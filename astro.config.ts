4acc4fec63d609dbbbd104ab28e48a7be75c45ec
Nikola Hristov <nikola@nikolahristov.tech>
2022-12-15 10:24:04 +0200

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -9,5 +9,11 @@ import prefetch from "@astrojs/prefetch";
 export default defineConfig({
 	// TODO Place your site URL here
 	// site: "",
-	integrations: [sitemap(), critters({ logger: 1 }), prefetch(), rome({ logger: 1 }), compress({ logger: 1 })],
+	integrations: [
+		sitemap(),
+		critters({ logger: 1 }),
+		prefetch(),
+		rome({ logger: 1 }),
+		compress({ logger: 1 }),
+	],
 });
