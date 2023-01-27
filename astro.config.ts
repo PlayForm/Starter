f2a3736f08473e6cacbc4f5213e6d998be6686c3
Nikola Hristov <nikola@nikolahristov.tech>
2023-01-27 02:23:20 +0200

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import critters from "astro-critters";
-import sitemap from "@astrojs/sitemap";
 import rome from "astro-rome";
-import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
 	// TODO Place your site URL here
