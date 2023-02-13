eb1ea406b026315227bbf8c501992b95c2628187
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-13 04:06:40 +0200

    Update Astro configuration & package.json to add service worker

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,18 @@
 // @ts-nocheck
 import { defineConfig } from "astro/config";
 
-import prefetch from "@astrojs/prefetch";
-import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
-import critters from "astro-critters";
 import rome from "astro-rome";
+import prefetch from "@astrojs/prefetch";
+import critters from "astro-critters";
+import sitemap from "@astrojs/sitemap";
+import worker from "astrojs-service-worker";
 
 export default defineConfig({
 	// TODO Place your site URL here
 	// site: "",
 	integrations: [
+		worker(),
 		sitemap(),
 		critters({ logger: 1 }),
 		prefetch(),
