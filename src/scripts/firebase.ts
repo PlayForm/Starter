f921b7683ef49a1b2305936fff7291f1ef249a64
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-21 03:24:30 +0200

    squash!

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-	apiKey: "",
-	appId: "",
-	authDomain: "",
-	databaseURL: "",
-	measurementId: "",
-	messagingSenderId: "",
-	projectId: "",
-	storageBucket: "",
+	apiKey: "", // TODO Replace with your API key
+	appId: "", // TODO Replace with your app ID
+	authDomain: "", // TODO Replace with your auth domain
+	databaseURL: "", // TODO Replace with your database URL
+	measurementId: "", // TODO Replace with your measurement ID
+	messagingSenderId: "", // TODO Replace with your messaging sender ID
+	projectId: "", // TODO Replace with your project ID
+	storageBucket: "", // TODO Replace with your storage bucket
 };
 
 initializeApp(firebaseConfig);
