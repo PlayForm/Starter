370749a264ed7943fd707878e2e007734c6ce31c
Nikola Hristov <nikola@nikolahristov.tech>
2023-03-28 21:27:50 +0300

    squash!

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,6 @@ module.exports = {
 	plugins: [
 		require("@tailwindcss/forms"),
 		require("@tailwindcss/typography"),
-		require("@tailwindcss/line-clamp"),
 		require("@tailwindcss/aspect-ratio"),
 	],
 };
