9052852b1dcc4c4a3abbf3405529ae0b5cb4beee
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-17 09:48:20 +0300

    squash!

--- Source/Library/Environment.ts
+++
@@ -1,9 +0,0 @@
-(await import("dotenv")).config();
-
-export default Zod.z
-	.object({
-		Token: Zod.z.string().default(""),
-	})
-	.parse(process.env);
-
-export const { default: Zod } = await import("zod");
