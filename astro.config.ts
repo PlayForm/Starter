92d590ff1dfde9a17e7506744afc8f4ae1c984d7
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-22 02:23:21 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -10,9 +10,9 @@ export default (await import("astro/config")).defineConfig({
 			? (await import("astrojs-service-worker")).default()
 			: null,
 		(await import("@astrojs/sitemap")).default(),
-		(await import("astro-critters")).default({ Logger: 1 }),
+		// (await import("astro-critters")).default({ Logger: 1 }),
 		(await import("@astrojs/prefetch")).default(),
-		(await import("astro-rome")).default({ Logger: 1 }),
+		// (await import("astro-rome")).default({ Logger: 1 }),
 		(await import("astro-compress")).default({ Logger: 1 }),
 	],
 	vite: {
