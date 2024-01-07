0b840727d0320c178e38ad6fd86449ed811d3313
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-07 03:47:22 +0200

    squash!

---
+++ Public/test.js
@@ -0,0 +1,14 @@
+// index.js
+function sayHello() {
+	console.log("Hello, world!");
+}
+
+function greet(name) {
+	console.log(`Greetings, ${name}!`);
+}
+
+const myNumber = 42;
+
+sayHello();
+greet("Alice");
+console.log(`My number is: ${myNumber}`);
