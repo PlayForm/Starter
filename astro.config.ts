8f6b3cf681b7d0fcf2319b79c4d84457b9b3c18a
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-19 14:14:42 +0200

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-export default (await import("astro/config")).defineConfig({
+export default ((await import("astro/config")).defineConfig({
 	srcDir: "./Source",
 	publicDir: "./Public",
 	outDir: "./Target",
@@ -20,6 +20,6 @@ export default (await import("astro/config")).defineConfig({
 			sourcemap: true,
 		},
 	},
-}) as typeof defineConfig;
+}) as typeof defineConfig);
 
 import type { defineConfig } from "astro/config";
